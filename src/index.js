fb230a5e9f8367410c13df63bfd5524edae3cfe1
Mikhail Grigoriev <mikhailgrigoriev92@gmail.com>
2022-06-13 16:24:02 -0400

    Bundle background image into index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import restaurantContent from './generatePage';
 import './style.css';
+import Background from './background.jpg';
 
  function component() {
    const content = document.getElementById('content');
-
    content.textContent = restaurantContent('Gri');
+
 
    return content;
  }
